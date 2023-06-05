50f7a015a5ee843c4705a365bc3a50e0f9c7c1e0
Dmitriy Biryulin <biryulin.develop@gmail.com>
2023-06-05 20:56:33 +0300

    add alarm

--- app/src/main/java/com/biryulindevelop/smartalarm/MainActivity.kt
+++ app/src/main/java/com/biryulindevelop/smartalarm/MainActivity.kt
@@ -1,10 +1,81 @@
 package com.biryulindevelop.smartalarm
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.icu.util.Calendar
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.biryulindevelop.smartalarm.databinding.ActivityMainBinding
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding by viewBinding(ActivityMainBinding::bind)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.alarmButton.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("Выберите время")
+                .build()
+
+            timePicker.addOnPositiveButtonClickListener {
+                val calendar = Calendar.getInstance()
+                calendar.set(Calendar.HOUR_OF_DAY, timePicker.hour)
+                calendar.set(Calendar.MINUTE, timePicker.minute)
+                calendar.set(Calendar.SECOND, 0)
+                calendar.set(Calendar.MILLISECOND, 0)
+
+                val alarmManager: AlarmManager =
+                    getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+                val alarmClockInfo =
+                    AlarmManager.AlarmClockInfo(calendar.timeInMillis, getAlarmInfoPendingIntent())
+
+                alarmManager.setAlarmClock(alarmClockInfo, getAlarmActionPendingIntent())
+
+                Toast.makeText(this, "Будильни установлен на ${timePicker.hour}:${timePicker.minute}", Toast.LENGTH_SHORT).show()
+            }
+            timePicker.show(supportFragmentManager, "timePicker")
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent =  Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+            }
+        }
+    }
+
+    private fun getAlarmInfoPendingIntent(): PendingIntent {
+        val alarmInfoIntent = Intent(this, MainActivity::class.java)
+        alarmInfoIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        return PendingIntent.getActivity(
+            this,
+            0,
+            alarmInfoIntent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
+
+    private fun getAlarmActionPendingIntent(): PendingIntent {
+        val intent = Intent(this, AlarmActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        return PendingIntent.getActivity(
+            this,
+            1,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
     }
 }
