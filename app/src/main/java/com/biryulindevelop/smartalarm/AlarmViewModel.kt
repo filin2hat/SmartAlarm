65c9c046915787f5cc972949a55fcbb166040540
Biryulin Dmitry <filin2hat@yandex.ru>
2023-06-06 14:35:29 +0300

    написал код базового будильника

---
+++ app/src/main/java/com/biryulindevelop/smartalarm/AlarmViewModel.kt
@@ -0,0 +1,19 @@
+package com.biryulindevelop.smartalarm
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+class AlarmViewModel : ViewModel() {
+    private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+
+    fun getTimeFlow(): Flow<String> = flow {
+        while (true) {
+            emit(LocalTime.now().format(timeFormatter))
+            kotlinx.coroutines.delay(1000)
+        }
+    }.flowOn(Dispatchers.IO)
+}
--- app/src/main/java/com/biryulindevelop/smartalarm/MainActivity.kt
+++ app/src/main/java/com/biryulindevelop/smartalarm/MainActivity.kt
@@ -6,53 +6,77 @@ import android.content.Context
 import android.content.Intent
 import android.icu.util.Calendar
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.biryulindevelop.smartalarm.databinding.ActivityMainBinding
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding by viewBinding(ActivityMainBinding::bind)
+    private val viewModel: AlarmViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.alarmButton.setOnClickListener {
-            val timePicker = MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_24H)
-                .setHour(12)
-                .setMinute(0)
-                .setTitleText("Выберите время")
-                .build()
+        checkOverlayPermission()
+    }
 
-            timePicker.addOnPositiveButtonClickListener {
-                val calendar = Calendar.getInstance()
-                calendar.set(Calendar.HOUR_OF_DAY, timePicker.hour)
-                calendar.set(Calendar.MINUTE, timePicker.minute)
-                calendar.set(Calendar.SECOND, 0)
-                calendar.set(Calendar.MILLISECOND, 0)
+    private fun checkOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, REQUEST_CODE_OVERLAY_PERMISSION)
+        } else {
+            setContentView(R.layout.activity_main)
 
-                val alarmManager: AlarmManager =
-                    getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-                val alarmClockInfo =
-                    AlarmManager.AlarmClockInfo(calendar.timeInMillis, getAlarmInfoPendingIntent())
-
-                alarmManager.setAlarmClock(alarmClockInfo, getAlarmActionPendingIntent())
-
-                Toast.makeText(this, "Будильни установлен на ${timePicker.hour}:${timePicker.minute}", Toast.LENGTH_SHORT).show()
+            lifecycleScope.launch {
+                viewModel.getTimeFlow().collect {
+                    binding.currentTimeTextView.text = it
+                }
             }
-            timePicker.show(supportFragmentManager, "timePicker")
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(this)) {
-                val intent =  Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-                startActivity(intent);
+
+            binding.alarmButton.setOnClickListener {
+                val timePicker = MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .setHour(12)
+                    .setMinute(0)
+                    .setTitleText("Выберите время")
+                    .build()
+
+                timePicker.addOnPositiveButtonClickListener {
+                    val calendar = Calendar.getInstance()
+                    calendar.set(Calendar.HOUR_OF_DAY, timePicker.hour)
+                    calendar.set(Calendar.MINUTE, timePicker.minute)
+                    calendar.set(Calendar.SECOND, 0)
+                    calendar.set(Calendar.MILLISECOND, 0)
+
+                    val alarmManager: AlarmManager =
+                        getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+                    val alarmClockInfo =
+                        AlarmManager.AlarmClockInfo(
+                            calendar.timeInMillis,
+                            getAlarmInfoPendingIntent()
+                        )
+
+                    alarmManager.setAlarmClock(alarmClockInfo, getAlarmActionPendingIntent())
+
+                    Toast.makeText(
+                        this,
+                        "Будильни установлен на ${timePicker.hour}:${timePicker.minute}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                timePicker.show(supportFragmentManager, "timePicker")
             }
         }
     }
@@ -77,5 +101,27 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             intent,
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         )
+    }
+
+    companion object {
+        private const val REQUEST_CODE_OVERLAY_PERMISSION = 123
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_OVERLAY_PERMISSION) {
+            if (Settings.canDrawOverlays(this)) {
+                checkOverlayPermission()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Разрешение на отображение наложений не предоставлено",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
     }
 }
